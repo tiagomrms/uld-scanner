d7d2d0bd3c720920c93f3c1b070a5cebb75f5071
tiagomrms <tiagomrms@gmail.com>
2026-09-09 21:40:21 +0100

    Corrige URL do Supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://lqrkgdmnkarmjdvkvkds.supabase.co/rest/v1/'
+const supabaseUrl = 'https://lqrkgdmnkarmjdvkvkds.supabase.co'
 const supabaseKey = 'sb_publishable_V-_IscTqQN7nrRKXWEazEA_JNnkIz2E'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
